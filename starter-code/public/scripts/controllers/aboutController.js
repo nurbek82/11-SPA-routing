02d8933c8190f0f3b8d70419de700921fa8f3af0
Nurbek Ismailov <nurbek82@gmail.com>
2017-05-01 10:08:45 -0700

    finished all todo, will test about page

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,7 +4,9 @@
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+  aboutController.index = function () {
+    $('#about').show().siblings().hide()
+  }
 
   module.aboutController = aboutController;
 })(window);
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// TODO: Configure the about and home routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+page('/', articleController.index);
+page('/about', aboutController.index);
+page();
